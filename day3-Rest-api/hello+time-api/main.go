a5f7e65e18a32242d3d3d7c1e65591818aa52a8c
Subash Nagarkoti <subashnagarkoti@Subashs-MacBook-Air.local>
2025-07-24 14:55:53 +0545

    adding comments to understand

--- day3-Rest-api/hello+time-api/main.go
+++ day3-Rest-api/hello+time-api/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// This function handles requests to /hello. Only allow GET requests (e.g. from browser or curl)
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "GET" {
@@ -17,6 +18,10 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// This function handles requests to /time.
+// Create a map (like a dictionary) to hold the current time.
+// Set the content type of the response to JSON.
+// Convert the map to JSON and write it to the response
 func timeHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "GET" {
@@ -29,6 +34,9 @@ func timeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(currentTime)
 }
+
+// Tell the server to run helloHandler when /hello is requested, and timeHandler when /time is requested.
+// start the server port on 8080
 func main() {
 
 	http.HandleFunc("/hello", helloHandler)
